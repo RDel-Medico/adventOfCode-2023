74544039b6f5216538065f5bbb9069175ec5ee65
RDel-Medico <redelmedico@gmail.com>
2023-12-02 06:37:14 +0100

    Day 2

--- day16/src/day16.java
+++ day16/src/day16.java
@@ -1,6 +1,21 @@
-public class day16 {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!\n");
+public class day16 {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("./src/input.txt"))) {
+            String line;
+
+            String[] elements = null;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                elements = line.split(".");
+            }
+
+            for (int i = 0; i < elements.length; i++) {
+                System.out.println(elements[i] + "\n");
+            }
+        }
     }
 }
--- day2/src/day2.java
+++ day2/src/day2.java
@@ -1,6 +1,65 @@
-public class day2 {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!\n");
+public class day2 {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("./src/input.txt"))) {
+            String line;
+
+            String[] games = new String[100];
+            int g = 0;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] temp;
+                temp = line.split(":");
+                games[g++] = temp[1];
+            }
+
+
+            String[] tirages;
+            String[] coup;
+            String[] temp;
+            int amount;
+            String couleur;
+
+            int rMin = 0;
+            int gMin = 0;
+            int bMin = 0;
+
+            int res = 0;
+            for (int i = 0; i < 100; i++) {
+                tirages = games[i].split(";");
+                for (int j = 0; j < tirages.length; j++) {
+                    coup = tirages[j].split(",");
+                    for (int k = 0; k < coup.length; k++) {
+                        temp = coup[k].split("[.]");
+
+                        amount = Integer.parseInt(temp[0]);
+                        couleur = temp[1];
+
+                        switch(couleur) {
+                            case "r":
+                                rMin = amount > rMin ? amount : rMin;
+                                break;
+                            case "g":
+                                gMin = amount > gMin ? amount : gMin;
+                                break;
+                            case "b":
+                                bMin = amount > bMin ? amount : bMin;
+                                break;
+                            default:
+                                break;
+                                
+                        }
+                    }
+                }
+                res = res + (rMin * bMin * gMin);
+                rMin = 0;
+                gMin = 0;
+                bMin = 0;
+            }
+            System.out.println("Res : " + res);
+        }   
     }
 }
