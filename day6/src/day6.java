65dd6394d265fc973647b24514e97e21ef3b052e
RDel-Medico <redelmedico@gmail.com>
2023-12-06 06:27:46 +0100

    Day 6

--- day6/src/day6.java
+++ day6/src/day6.java
@@ -1,21 +1,21 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
 
 public class day6 {
     public static void main(String[] args) throws IOException {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("./src/input.txt"))) {
-            String line;
+            int nbSolution = 0;
+            int time = 47707566;
+            BigInteger timeTobeat = new BigInteger("282107911471062");
 
-            String[] elements = null;
-
-            while ((line = bufferedReader.readLine()) != null) {
-                elements = line.split(".");
+            for (int j = 0; j < time; j++) {
+                if (-1 == timeTobeat.divide(new BigInteger(Integer.toString(time-j))).compareTo(new BigInteger(Integer.toString(j)))) {
+                    nbSolution++;
+                }
             }
-
-            for (int i = 0; i < elements.length; i++) {
-                System.out.println(elements[i] + "\n");
-            }
+            System.out.println("res : " + nbSolution);
         }
     }
 }
